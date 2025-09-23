2a47787dcb7ada750a9430e2a26b18d7b4f4eff1
rakannaufal <naufalrakan432@gmail.com>
2025-09-23 21:47:34 +0700

    fifth commit

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -16,7 +16,7 @@ const financeStore = useFinanceStore();
 const themeStore = useThemeStore();
 const router = useRouter();
 
-const appVersion = "1.2.1";
+const appVersion = "1.2.2";
 
 const currentLogo = computed(() => {
   return themeStore.theme === "light" ? logoDark : logoLight;
